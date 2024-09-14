0edcfae162eccebbc914eabd80283d1b7b0246cc
vansh sharma <vanshsharma5503@gmail.com>
2024-09-14 19:56:19 +0530

    Initial Commit

---
+++ SnoozzZY/SnoozzZYApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnoozzZYApp.swift
+//  SnoozzZY
+//
+//  Created by Vansh Sharma on 14/09/24.
+//
+
+import SwiftUI
+
+@main
+struct SnoozzZYApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
